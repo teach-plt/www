6ffa5fc71d942f4a7af5a5d2777a47abfa812fe5
Andreas Abel <andreas.abel@gu.se>
2015-10-13 14:27:51 +0200

    Script to set correct access control lists for DAT151 web page

---
+++ fix-acls.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+CONF=/chalmers/sw/sup64/fix_personal_www_rights-1.1/etc
+cd /chalmers/groups/edu2009/www/www.cse.chalmers.se/year/2015/course/DAT151
+find . -type d                -print0 | xargs -r -0 nfs4_setfacl -S ${CONF}/dir.acl
+find . -type f   -perm /ugo+x -print0 | xargs -r -0 nfs4_setfacl -S ${CONF}/xfile.acl
+find . -type f ! -perm /ugo+x -print0 | xargs -r -0 nfs4_setfacl -S ${CONF}/file.acl
+
+# EOF
