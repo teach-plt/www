0cdd95dac59e903de7febf8dceef2dab8d999989
Andreas Abel <andreas.abel@ifi.lmu.de>
2016-10-26 20:30:53 +0200

    Updated fix-acls.sh to 2016.

--- fix-acls.sh
+++ fix-acls.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 CONF=/chalmers/sw/sup64/fix_personal_www_rights-1.1/etc
-cd /chalmers/groups/edu2009/www/www.cse.chalmers.se/year/2015/course/DAT151
+cd /chalmers/groups/edu2009/www/www.cse.chalmers.se/year/2016/course/DAT151
 find . -type d                -print0 | xargs -r -0 nfs4_setfacl -S ${CONF}/dir.acl
 find . -type f   -perm /ugo+x -print0 | xargs -r -0 nfs4_setfacl -S ${CONF}/xfile.acl
 find . -type f ! -perm /ugo+x -print0 | xargs -r -0 nfs4_setfacl -S ${CONF}/file.acl
