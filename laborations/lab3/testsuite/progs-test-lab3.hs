0d9fb11b5d033f38a6d714cf8b73d18d31fc9723
Andreas Abel <andreas.abel@ifi.lmu.de>
2016-11-24 22:00:54 +0100

    [ lab3 testsuite ] Whitespace in progs-test-lab3.hs

--- laborations/lab3/testsuite/progs-test-lab3.hs
+++ laborations/lab3/testsuite/progs-test-lab3.hs
@@ -4,17 +4,18 @@
 
 import Control.Concurrent
 import Control.Monad
+
 import Data.Char
 import Data.IORef
 import Data.List
 import Data.Maybe
+
 import System.Directory
 import System.Environment
 import System.Exit
 import System.IO
 import System.Process
 import System.IO.Unsafe
-
 import System.FilePath.Posix (takeBaseName, takeDirectory)
 
 #if __GLASGOW_HASKELL__ >= 706
@@ -38,8 +39,9 @@ doDebug :: IORef Bool
 doDebug = unsafePerformIO $ newIORef False
 
 debug :: String -> IO ()
-debug s = do d <- readIORef doDebug
-             if d then putStrLn s else return ()
+debug s = do
+  d <- readIORef doDebug
+  if d then putStrLn s else return ()
 
 
 listGoodProgs = listCCFiles "good"
@@ -49,65 +51,66 @@ listCCFiles dir =
 
 
 welcome :: IO ()
-welcome = do putStrLn $ "This is the (reduced) test program for Programming Languages Lab 3"
+welcome = putStrLn $ "This is the (reduced) test program for Programming Languages Lab 3"
 
 
 runMake :: FilePath -> IO ()
-runMake dir = do checkDirectoryExists dir
-                 runCommandNoFail_ ("make -C " ++ quote dir) ""
+runMake dir = do
+  checkDirectoryExists dir
+  runCommandNoFail_ ("make -C " ++ quote dir) ""
 
 runTests :: FilePath -> IO [Bool]
-runTests dir =
-    do let prog = joinPath [dir,executable_name]
-       checkFileExists prog
-       goodProgs <- listGoodProgs
-       good <- mapM (testBackendProg prog) goodProgs
-       return good
+runTests dir = do
+  let prog = joinPath [dir,executable_name]
+  checkFileExists prog
+  goodProgs <- listGoodProgs
+  good <- mapM (testBackendProg prog) goodProgs
+  return good
 
 
 testBackendProg :: FilePath -> FilePath -> IO Bool
-testBackendProg prog f =
-    do input  <- readFileIfExists (f++".input")
-       output <- readFileIfExists (f++".output")
+testBackendProg prog f = do
+  input  <- readFileIfExists (f++".input")
+  output <- readFileIfExists (f++".output")
 
-       -- Code Gen
-       let c = prog ++ " " ++ f
-       putStrLn $ "Running " ++ f ++ "..."
-       (out,err,s) <- runCommandStrWait c ""
+  -- Code Gen
+  let c = prog ++ " " ++ f
+  putStrLn $ "Running " ++ f ++ "..."
+  (out,err,s) <- runCommandStrWait c ""
 
-       -- Run code
-       let c = "java -noverify -cp .:" ++ takeDirectory f ++ " " ++ takeBaseName f
-       -- let c = "java -cp .:" ++ takeDirectory f ++ " " ++ takeBaseName f
-       (out,err,s) <- runCommandStrWait c input
-       debug $ "Exit code: " ++ show s
-       if out == output
-         then return True
-         else do reportError c "invalid output" f input out err
-		 putStrLn "Expected output:"
-		 putStrLn $ color blue $ output
-                 return False
+  -- Run code
+  let c = "java -noverify -cp .:" ++ takeDirectory f ++ " " ++ takeBaseName f
+  -- let c = "java -cp .:" ++ takeDirectory f ++ " " ++ takeBaseName f
+  (out,err,s) <- runCommandStrWait c input
+  debug $ "Exit code: " ++ show s
+  if out == output then return True else do
+    reportError c "invalid output" f input out err
+    putStrLn "Expected output:"
+    putStrLn $ color blue $ output
+    return False
 
 --
 -- * Main
 --
 
 parseArgs :: [String] -> IO String
-parseArgs ["-debug",cfFile] =
-    do writeIORef doDebug True
-       return cfFile
+parseArgs ["-debug",cfFile] = do
+  writeIORef doDebug True
+  return cfFile
 parseArgs [cfFile] = return cfFile
-parseArgs _ = do hPutStrLn stderr "Usage: progs-test-lab3 <interpreter code directory>"
-                 exitFailure
+parseArgs _ = do
+  hPutStrLn stderr "Usage: progs-test-lab3 <interpreter code directory>"
+  exitFailure
 
 mainOpts :: FilePath -> IO ()
-mainOpts dir =
-    do welcome
-       runCommandNoFail_ "rm -f */*.j */*.class" ""
-       runMake dir
-       good <- runTests dir
-       putStrLn ""
-       putStrLn "------------------------------------------------------------"
-       report "Good programs: " good
+mainOpts dir = do
+  welcome
+  runCommandNoFail_ "rm -f */*.j */*.class" ""
+  runMake dir
+  good <- runTests dir
+  putStrLn ""
+  putStrLn "------------------------------------------------------------"
+  report "Good programs: " good
 
 main :: IO ()
 main = getArgs >>= parseArgs >>= mainOpts
@@ -184,135 +187,145 @@ bgcol col = "\ESC[0" ++ show (40+col) ++ "m"
 
 red, green, blue, black :: Color
 black = 0
-red = 1
+red   = 1
 green = 2
-blue = 6
+blue  = 6
 
 --
 -- * Various versions of runCommand
 --
 
-runCommandStr :: String -- ^ command
-	      -> String -- ^ stdin data
-	      -> IO (String,String,ProcessHandle) -- ^ stdout, stderr, process
-runCommandStr c inStr =
-    do
-    outVar <- newEmptyMVar
-    errVar <- newEmptyMVar
-    (pin,pout,perr,p) <- runInteractiveCommand c
-    forkIO $ do debug "Writing input..."
-                hPutStr pin inStr
-                hClose pin
-                debug "Wrote input."
-    forkIO $ do debug "Reading output..."
-                s <- hGetContents pout
-                putMVar outVar s
-                debug "Read output."
-    forkIO $ do debug "Reading error..."
-                s <- hGetContents perr
-                putMVar errVar s
-                debug "Read error."
-    out <- takeMVar outVar
-    err <- takeMVar errVar
-    return (out,err,p)
+runCommandStr
+  :: String                           -- ^ command
+  -> String                           -- ^ stdin data
+  -> IO (String,String,ProcessHandle) -- ^ stdout, stderr, process
+runCommandStr c inStr = do
+  outVar <- newEmptyMVar
+  errVar <- newEmptyMVar
+  (pin,pout,perr,p) <- runInteractiveCommand c
+
+  forkIO $ do
+    debug "Writing input..."
+    hPutStr pin inStr
+    hClose pin
+    debug "Wrote input."
+
+  forkIO $ do
+    debug "Reading output..."
+    s <- hGetContents pout
+    putMVar outVar s
+    debug "Read output."
+
+  forkIO $ do
+    debug "Reading error..."
+    s <- hGetContents perr
+    putMVar errVar s
+    debug "Read error."
+
+  out <- takeMVar outVar
+  err <- takeMVar errVar
+  return (out,err,p)
 
 
-runCommandStrWait :: String -- ^ command
-		  -> String -- ^ stdin data
-		  -> IO (String,String,ExitCode) -- ^ stdout, stderr, process exit status
-runCommandStrWait c inStr =
-    do
-    debug $ "Running " ++ c
-    (out,err,p) <- runCommandStr c inStr
-    s <- waitForProcess p
-    debug $ "Standard output:\n" ++ out
-    debug $ "Standard error:\n" ++ err
-    return (out,err,s)
+runCommandStrWait
+  :: String                      -- ^ command
+  -> String                      -- ^ stdin data
+  -> IO (String,String,ExitCode) -- ^ stdout, stderr, process exit status
+runCommandStrWait c inStr = do
+  debug $ "Running " ++ c
+  (out,err,p) <- runCommandStr c inStr
+  s <- waitForProcess p
+  debug $ "Standard output:\n" ++ out
+  debug $ "Standard error:\n" ++ err
+  return (out,err,s)
 
-runCommandNoFail_ :: String -- ^ Command
-                  -> FilePath -- ^ Input file
-                  -> IO ()
+runCommandNoFail_
+  :: String   -- ^ Command
+  -> FilePath -- ^ Input file
+  -> IO ()
 runCommandNoFail_ c f = runCommandNoFail c f >> return ()
 
-runCommandNoFail :: String -- ^ Command
-                 -> FilePath -- ^ Input file
-                 -> IO (String,String) -- ^ stdout and stderr
-runCommandNoFail e f =
-    do
-    let c = e ++ " " ++ f
-    hPutStrLn stderr $ "Running " ++ c ++ "..."
-    (out,err,s) <- runCommandStrWait c ""
-    case s of
-	   ExitFailure x -> do
-			    reportError e ("with status " ++ show x) f "" out err
-			    exitFailure
-	   ExitSuccess -> return (out,err)
+runCommandNoFail
+  :: String             -- ^ Command
+  -> FilePath           -- ^ Input file
+  -> IO (String,String) -- ^ stdout and stderr
+runCommandNoFail e f = do
+  let c = e ++ " " ++ f
+  hPutStrLn stderr $ "Running " ++ c ++ "..."
+  (out,err,s) <- runCommandStrWait c ""
+  case s of
+    ExitFailure x -> do
+      reportError e ("with status " ++ show x) f "" out err
+      exitFailure
+    ExitSuccess -> return (out,err)
 
 --
 -- * Checking files and directories
 --
 
 checkFileExists :: FilePath -> IO ()
-checkFileExists f =
-    do e <- doesFileExist f
-       when (not e) $ do putStrLn $ color red $ quote f ++ " is not an existing file."
-		         exitFailure
+checkFileExists f = do
+   e <- doesFileExist f
+   when (not e) $ do
+     putStrLn $ color red $ quote f ++ " is not an existing file."
+     exitFailure
 
 checkDirectoryExists :: FilePath -> IO ()
-checkDirectoryExists f =
-    do e <- doesDirectoryExist f
-       when (not e) $ do putStrLn $ color red $ quote f ++ " is not an existing directory."
-		         exitFailure
+checkDirectoryExists f = do
+  e <- doesDirectoryExist f
+  when (not e) $ do
+    putStrLn $ color red $ quote f ++ " is not an existing directory."
+    exitFailure
 
 --
 -- * Error reporting and output checking
 --
 
-reportErrorColor :: Color
-                 -> String -- ^ command that failed
-	         -> String -- ^ how it failed
-	         -> FilePath -- ^ source file
-	         -> String -- ^ given input
-	         -> String -- ^ stdout output
-	         -> String -- ^ stderr output
-	         -> IO ()
-reportErrorColor col c m f i o e =
-    do
+reportErrorColor
+  :: Color
+  -> String   -- ^ command that failed
+  -> String   -- ^ how it failed
+  -> FilePath -- ^ source file
+  -> String   -- ^ given input
+  -> String   -- ^ stdout output
+  -> String   -- ^ stderr output
+  -> IO ()
+reportErrorColor col c m f i o e = do
     putStrLn $ color col $ c ++ " failed: " ++ m
     when (not (null f)) $ prFile f
     when (not (null i)) $ do
-			  putStrLn "Given this input:"
-			  putStrLn $ color blue $ i
+      putStrLn "Given this input:"
+      putStrLn $ color blue $ i
     when (not (null o)) $ do
-			  putStrLn "It printed this to standard output:"
-			  putStrLn $ color blue $ o
+      putStrLn "It printed this to standard output:"
+      putStrLn $ color blue $ o
     when (not (null e)) $ do
-			  putStrLn "It printed this to standard error:"
-			  putStrLn $ color blue $ e
+      putStrLn "It printed this to standard error:"
+      putStrLn $ color blue $ e
 
-reportError :: String -- ^ command that failed
-	    -> String -- ^ how it failed
-	    -> FilePath -- ^ source file
-	    -> String -- ^ given input
-	    -> String -- ^ stdout output
-	    -> String -- ^ stderr output
-	    -> IO ()
+reportError
+  :: String   -- ^ command that failed
+  -> String   -- ^ how it failed
+  -> FilePath -- ^ source file
+  -> String   -- ^ given input
+  -> String   -- ^ stdout output
+  -> String   -- ^ stderr output
+  -> IO ()
 reportError = reportErrorColor red
 
 prFile :: FilePath -> IO ()
 prFile f = do
-           e <- doesFileExist f
-           when e $ do putStrLn $ "For input file " ++ f ++ ":"
-	               putStrLn $ "---------------- begin " ++ f ++ " ------------------"
-	               s <- readFile f
-	               putStrLn $ color green s
-	               putStrLn $ "----------------- end " ++ f ++ " -------------------"
-
+  e <- doesFileExist f
+  when e $ do
+    putStrLn $ "For input file " ++ f ++ ":"
+    putStrLn $ "---------------- begin " ++ f ++ " ------------------"
+    s <- readFile f
+    putStrLn $ color green s
+    putStrLn $ "----------------- end " ++ f ++ " -------------------"
 
 -- | Report how many tests passed.
 report :: String -> [Bool] -> IO ()
-report n rs =
-  do let (p,t) = (length (filter id rs), length rs)
-         c = if p == t then green else red
-     putStrLn $ color c $
-              n ++ "passed " ++ show p ++ " of " ++ show t ++ " tests"
+report n rs = do
+  let (p,t) = (length (filter id rs), length rs)
+      c     = if p == t then green else red
+  putStrLn $ color c $ n ++ "passed " ++ show p ++ " of " ++ show t ++ " tests"
