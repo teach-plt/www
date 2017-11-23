8fe7d1af8d064b5a387d56255368d2423134cfcf
Andreas Lööw <loow@chalmers.se>
2017-11-23 20:07:40 +0100

    Ops, does not work for e.g. Cygwin users

--- laborations/lab3/testsuite/build-tarball.sh
+++ laborations/lab3/testsuite/build-tarball.sh
@@ -5,9 +5,9 @@ NAME=lab3-testsuite
 mkdir "$NAME"
 mkdir "$NAME/good"
 mkdir -p "$NAME/dir-for-path-test/one-more-dir"
-mkdir "$NAME/bin"
 
 cp -p progs-test-lab3.hs "$NAME"
+cp -p jasmin.jar         "$NAME"
 cp -p Runtime.java       "$NAME"
 cp -p Makefile-test      "$NAME/Makefile"
 cp -p index.txt          "$NAME"
@@ -15,8 +15,6 @@ cp -p good/*.cc        "$NAME/good/"
 cp -p good/*.cc.input  "$NAME/good/"
 cp -p good/*.cc.output "$NAME/good/"
 cp -p dir-for-path-test/one-more-dir/simple.cc "$NAME/dir-for-path-test/one-more-dir/"
-cp -p bin/jasmin        "$NAME/bin/"
-cp -p bin/jasmin.jar    "$NAME/bin/"
 
 tar -zcf "$NAME.tar.gz" "$NAME"
 
