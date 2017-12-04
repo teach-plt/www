eff5c1800f90a0aeff78a15ecf9fe8faf0062b9f
Fabian <bafain@gmail.com>
2017-12-04 12:44:13 +0100

    [ testsuite4 ] Stick to directory layout

--- laborations/lab4/testsuite/build-tarball.sh
+++ laborations/lab4/testsuite/build-tarball.sh
@@ -3,9 +3,12 @@
 NAME=lab4-testsuite
 
 mkdir "$NAME"
+mkdir "$NAME/bad"
+mkdir "$NAME/good"
 
-cp progs-test-lab4.sh "$NAME"
-cp -p *.hs "$NAME"
+cp progs-test-lab4 "$NAME"
+cp -p bad/*.hs "$NAME/bad/"
+cp -p good/*.hs "$NAME/good/"
 
 tar -zcf "$NAME.tar.gz" "$NAME"
 
