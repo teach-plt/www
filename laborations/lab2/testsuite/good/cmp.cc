ef49b0c0c5ed6401bc3eed554952fea026c5b8dd
Fabian <bafain@gmail.com>
2018-01-05 14:48:49 +0100

    Test > (>=) is not implemented as the negation of < (<=)
    (cherry picked from commit 1486915e21a13fdd822ff17b1c51e36f70270137)

--- laborations/lab2/testsuite/good/cmp.cc
+++ laborations/lab2/testsuite/good/cmp.cc
@@ -2,9 +2,13 @@ int main() {
   printBool(40 == 40);
   printBool(40 != 40);
   printBool(3 > 4);
+  printBool(3 > 3);
   printBool(4 < 3);
+  printBool(4 < 4);
   printBool(400 >= 300);
+  printBool(400 >= 400);
   printBool(400 <= 300);
+  printBool(400 <= 400);
 }
 
 void printBool(bool b) {
