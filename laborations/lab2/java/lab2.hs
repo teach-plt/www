7593420393f090b4ce28ac7a171b3e499f35d611
Andreas Abel <andreas.abel@ifi.lmu.de>
2018-12-03 21:37:29 +0100

    [ lab 2 ] Java: make lab2 runner generic

--- laborations/lab2/java/lab2.hs
+++ laborations/lab2/java/lab2.hs
@@ -12,8 +12,14 @@ import System.Process
 javaProg :: String
 javaProg = "java"
 
-className :: String
-className = "lab2"
+-- | The base name of the class file to run is assumed to have
+--   the same name as this runner.
+
+getClassName :: IO String
+getClassName = dropExtension <$> getProgName
+
+-- Should this not work, then one may give the name of the class directly, e.g.
+-- getClassName = return "lab2"
 
 -- | The class path separator is OS-specific:
 --   ';' on Windows, ':' on linux/Unix/MacOSX.
@@ -37,6 +43,7 @@ main :: IO ()
 main = do
   args      <- getArgs
   dir       <- dropFileName <$> getExecutablePath
+  className <- getClassName
   classPath <- fromMaybe "" <$> lookupEnv "CLASSPATH"
   let cp      = dir <:> classPath
       cmdArgs = ["-cp",cp,className] ++ args
