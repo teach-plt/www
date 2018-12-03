a05ae5e7481a7d4b10b17ab4e783d5c0676e9393
Andreas Abel <andreas.abel@ifi.lmu.de>
2018-12-03 20:54:02 +0100

    [ lab 2 ] Java: commented lab2 runner

--- laborations/lab2/java/lab2.hs
+++ laborations/lab2/java/lab2.hs
@@ -1,3 +1,5 @@
+-- | OS-agnostic runner for PLT lab2 written in Haskell.
+
 module Main where
 
 import Data.Maybe
@@ -11,6 +13,9 @@ javaProg = "java"
 className :: String
 className = "lab2"
 
+-- | The class path separator is OS-specific:
+--   ';' on Windows, ':' on linux/Unix/MacOSX.
+
 classpathSeparator :: Char
 #if defined(mingw32_HOST_OS)
 classpathSeparator = ';'
@@ -21,10 +26,11 @@ classpathSeparator = ':'
 (<:>) :: String -> String -> String
 s <:> t = s ++ [classpathSeparator] ++ t
 
+main :: IO ()
 main = do
   args      <- getArgs
   dir       <- dropFileName <$> getExecutablePath
-  classPath <- fromMaybe [] <$> lookupEnv "CLASSPATH"
+  classPath <- fromMaybe "" <$> lookupEnv "CLASSPATH"
   let cp      = dir <:> classPath
       cmdArgs = ["-cp",cp,className] ++ args
       cmdLine = showCommandForUser javaProg cmdArgs
