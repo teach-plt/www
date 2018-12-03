b36ddcc3cae89035ca4e708cbd564c9661dfa35d
Andreas Abel <andreas.abel@ifi.lmu.de>
2018-12-03 20:54:39 +0100

    [ lab 2 ] Java: restored simple shell runner for lab2

---
+++ laborations/lab2/java/lab2.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# A simple shell program to run PLT lab2 on linux/Unix systems.
+
+# Adds the current dir to the class path so that lab2.class
+# is found even when called from another directory.
+
+# Rename this to  lab2  to use with the testsuite.
+
+dir=`dirname $0`
+exec java -cp "$dir:$CLASSPATH" lab2 "$@"
