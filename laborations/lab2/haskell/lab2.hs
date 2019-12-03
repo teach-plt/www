a7a8320a6ce8d8365e65608f06642ccddde2fa26
Fabian <bafain@gmail.com>
2019-12-03 18:11:09 +0100

    [ lab2 ] Handle fail instead of error calls
    - error denotes a programming error
    
    - fail raises IOException of "type" UserError
    - error raises ErrorCall

--- laborations/lab2/haskell/lab2.hs
+++ laborations/lab2/haskell/lab2.hs
@@ -2,6 +2,7 @@ import Control.Exception
 
 import System.Environment (getArgs)
 import System.Exit (exitFailure)
+import System.IO.Error (isUserError, ioeGetErrorString)
 
 import CMM.Lex
 import CMM.Par
@@ -25,8 +26,8 @@ check s = do
           putStrLn "TYPE ERROR"
           putStrLn err
           exitFailure
-        Ok _ -> interpret tree `catch` \e -> case e of
-          ErrorCall err -> do
+        Ok _ -> catchJust (\e -> if isUserError e then Just (ioeGetErrorString e) else Nothing) (interpret tree) $
+          \err -> do
             putStrLn "INTERPRETER ERROR"
             putStrLn err
             exitFailure
