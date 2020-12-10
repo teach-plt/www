56794e50c6a2f1a79fac506419dec40c892e90cd
Andreas Abel <andreas.abel@ifi.lmu.de>
2020-12-10 12:08:50 +0100

    [ lab4, java ] cosmetic fixes to lab4.java

--- laborations/lab4/java/lab4.java
+++ laborations/lab4/java/lab4.java
@@ -5,15 +5,20 @@ import java.io.*;
 public class lab4 {
 
     public static void usage() {
-        System.err.println("Usage: lab4 <SourceFile>");
+        System.err.println("Usage: lab4 [-n|-v] FILE");
+        System.err.println("Interprets FILE either in call-by-name (-n) or call-by-value (-v, default).");
         System.exit(1);
     }
 
     public static void main(String args[]) {
+
+        // Parse command line
+
         String file = null;
         Strategy strategy = Strategy.CallByValue;  // call-by-value is default strategy
         for (String arg : args) {
             switch (arg) {
+
                 case "-n":
                     strategy = Strategy.CallByName;
                     break;
@@ -23,32 +28,42 @@ public class lab4 {
                     break;
 
                 default:
-                    file = arg;
-                    break;
+                    if (file == null) file = arg;
+                    else usage();
             }
         }
         if (file == null) usage();
 
+        // Run interpreter
+
         Yylex l = null;
         try {
+
             l = new Yylex(new FileReader(file));
             parser p = new parser(l);
             Program prg = p.pProgram();
             new Interpreter(strategy).interpret(prg);
+
         } catch (RuntimeException e) {
+
             System.out.println("INTERPRETER ERROR");
             System.err.println(e.toString());
             System.exit(-1);
+
         } catch (IOException e) {
+
             System.err.println(e.toString());
             System.exit(1);
+
         } catch (Throwable e) {
+
             System.out.println("SYNTAX ERROR");
             System.out.println("At line " + String.valueOf(l.line_num())
                        + ", near \"" + l.buff() + "\" :");
             System.out.println("     " + e.getMessage());
             e.printStackTrace();
             System.exit(1);
+
         }
     }
 }
