dd808ea7307b266c6e074653b3c278f0e7a4ef8f
Andreas Abel <andreas.abel@ifi.lmu.de>
2021-08-23 17:38:12 +0200

    [ lab1 testsuite ] nesting if-expressions

--- laborations/lab1/testsuite/good/good020-if-expr.cc
+++ laborations/lab1/testsuite/good/good020-if-expr.cc
@@ -1,1 +1,2 @@
-int x = 1 ? 0 : 2+3;
+// Andreas, 2021-04-30, test nesting of if expressions _?_:_ and throw.
+int x = 1 ? 0 ? throw throw x && y ? 0<1 : 1<0 : 6 < 7 : 2+3 ? a::b::c -= 4 > 5 : d::e::f += x = y;
