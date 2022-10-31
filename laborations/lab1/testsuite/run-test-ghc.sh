86cceee79a59c38cc6201129b3e95a4fb092fdb8
Andreas Abel <andreas.abel@ifi.lmu.de>
2022-10-31 19:39:28 +0100

    [ lab 1 testsuite ] add runner scripts via runghc, cabal, and stack

---
+++ laborations/lab1/testsuite/run-test-ghc.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "$1" == "" -o  "$1" == "-h" -o "$1" == "--help" ]; then
+  echo "PLT lab 1 testsuite runner"
+  echo "usage: $0 GRAMMARFILE"
+  exit 1
+fi
+
+runghc plt-test-lab1 -- "$1"
+
+# EOF
