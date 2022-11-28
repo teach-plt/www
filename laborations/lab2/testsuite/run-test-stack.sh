7681222af43d7a8837db3b74ce5a94525d3310fc
Andreas Abel <andreas.abel@ifi.lmu.de>
2022-11-28 23:41:12 +0100

    [ lab2 testsuite ] .sh scripts to run testsuite

---
+++ laborations/lab2/testsuite/run-test-stack.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "$1" == "" -o  "$1" == "-h" -o "$1" == "--help" ]; then
+  echo "PLT lab 2 testsuite runner"
+  echo "usage: $0 DIRECTORY"
+  exit 1
+fi
+
+stack run -- "$1"
+
+# EOF
