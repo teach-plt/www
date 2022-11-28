d85e1cf14262f669adcbee1b7bd2b3faf68e5c9c
Andreas Abel <andreas.abel@ifi.lmu.de>
2022-11-28 23:59:17 +0100

    [ lab3 testsuite ] add .sh scripts to run testsuite

---
+++ laborations/lab3/testsuite/run-test-ghc.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+if [ "$1" == "" -o  "$1" == "-h" -o "$1" == "--help" ]; then
+  echo "PLT lab 3 testsuite runner"
+  echo "usage: $0 DIRECTORY"
+  exit 1
+fi
+
+runghc plt-test-lab3 -- "$1"
+
+# EOF
