0ea54f8bddcb48e530035d81ca6fed70f3775e9a
Andreas Abel <andreas.abel@ifi.lmu.de>
2023-10-23 17:13:16 +0200

    [ lab2, haskell ] call BNFC in Setup.hs; add Haskell CI

---
+++ laborations/lab2/haskell/Setup.hs
@@ -0,0 +1,19 @@
+-- | Hook BNFC into the build process to generate AST, lexer, parser, and printer definitions.
+
+import Distribution.Simple            (defaultMainWithHooks, simpleUserHooks, buildHook)
+import Distribution.Simple.BuildPaths (autogenPackageModulesDir)
+import System.Process                 (callProcess)
+
+main :: IO ()
+main = defaultMainWithHooks simpleUserHooks
+  { buildHook = \ packageDescription localBuildInfo userHooks buildFlags -> do
+      -- For simplicity, generate files in build/global-autogen;
+      -- there they are available to all components of the package.
+      callProcess "bnfc"
+        [ "-o", autogenPackageModulesDir localBuildInfo
+        , "-d"
+        , "CMM.cf"
+        ]
+      -- Run the build process.
+      buildHook simpleUserHooks packageDescription localBuildInfo userHooks buildFlags
+  }
