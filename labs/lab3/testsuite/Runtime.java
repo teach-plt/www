0d85c85b9aeee69c0da400765c818a5364725dde
Andreas Abel <andreas.abel@ifi.lmu.de>
2024-05-28 11:49:21 +0200

    [ lab3 ] Runtime.java: force decimal dot by using Locale.ROOT

--- labs/lab3/testsuite/Runtime.java
+++ labs/lab3/testsuite/Runtime.java
@@ -1,15 +1,18 @@
+import java.lang.String;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Runtime {
 
-    private static Scanner scan = new Scanner(System.in);
+    private static Scanner scan = (new Scanner(System.in)).useLocale(Locale.ROOT);
 
     public static void printInt (int n) {
         System.out.println(n);
     }
 
+    // Use the ROOT locale to force a decimal dot rather than the country-specific decimal separator.
     public static void printDouble (double x) {
-        System.out.println(x);
+        System.out.println(String.format(Locale.ROOT, "%f", x));
     }
 
     public static int readInt () {
