613ff9957de4f134c8237ce19756b5b28746e3fa
Andreas Abel <andreas.abel@ifi.lmu.de>
2026-09-03 17:46:47 +0200

    Fix Setup.hs for Cabal >= 3.14

--- labs/lab2/haskell/Setup.hs
+++ labs/lab2/haskell/Setup.hs
@@ -1,23 +1,42 @@
 -- Programming Language Technology (Chalmers DAT151 / GU DIT231).
--- (C) Andreas Abel 2023
+-- (C) Andreas Abel 2023-25
 -- All rights reserved.
+
+{-# LANGUAGE CPP #-}
 
 -- | Hook BNFC into the cabal build process to generate AST, lexer, parser, and printer definitions.
 
-import Distribution.Simple            (defaultMainWithHooks, simpleUserHooks, buildHook)
+import Distribution.Simple            (defaultMainWithHooks, simpleUserHooks, buildHook, replHook)
 import Distribution.Simple.BuildPaths (autogenPackageModulesDir)
+#if MIN_VERSION_Cabal(3,14,0)
+import Distribution.Utils.Path        (interpretSymbolicPath)
+#endif
 import System.Process                 (callProcess)
 
 main :: IO ()
-main = defaultMainWithHooks simpleUserHooks
-  { buildHook = \ packageDescription localBuildInfo userHooks buildFlags -> do
+main = do
+  defaultMainWithHooks simpleUserHooks
+    { buildHook = \ packageDescription localBuildInfo userHooks buildFlags -> do
+        -- Call BNFC.
+        callBNFC localBuildInfo
+        -- Run the build process.
+        buildHook simpleUserHooks packageDescription localBuildInfo userHooks buildFlags
+    , replHook = \ packageDescription localBuildInfo userHooks buildFlags replArgs -> do
+        -- Call BNFC.
+        callBNFC localBuildInfo
+        -- Run the repl.
+        replHook simpleUserHooks packageDescription localBuildInfo userHooks buildFlags replArgs
+    }
+  where
+    callBNFC localBuildInfo = do
       -- For simplicity, generate files in build/global-autogen;
       -- there they are available to all components of the package.
       callProcess "bnfc"
-        [ "-o", autogenPackageModulesDir localBuildInfo
+        [ "-o",
+#if MIN_VERSION_Cabal(3,14,0)
+          interpretSymbolicPath Nothing $
+#endif
+            autogenPackageModulesDir localBuildInfo
         , "-d"
         , "CMM.cf"
         ]
-      -- Run the build process.
-      buildHook simpleUserHooks packageDescription localBuildInfo userHooks buildFlags
-  }
